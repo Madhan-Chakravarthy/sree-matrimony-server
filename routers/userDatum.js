0829d7ae0e58ed28c67cb7a8321646bf344301a9
MadhanChakravarthy <madhanchakravarthy1999@gmail.com>
2021-10-13 17:14:47 +0530

    bcryptjs installed bcrypt uninstalled

--- routers/userDatum.js
+++ routers/userDatum.js
@@ -3,7 +3,7 @@ const {UserData} = require('../models/userData')
 const express  = require('express');
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
 
